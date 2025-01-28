4b97d43ca8449c7fe66fc72481ea55f7f42bcdca
N.Abdi <narimanabdi@gmail.com>
2025-01-28 12:07:33 +0330

    solve issue #1

--- tests/unit/test_area.py
+++ tests/unit/test_area.py
@@ -3,7 +3,8 @@ from src.area import calculate_area_square
 
 def test_calculate_area_square():  
     assert calculate_area_square(2) == 4  
-    assert calculate_area_square(2.5) == 6.25  
+    assert calculate_area_square(2.5) == 6.25
+    assert calculate_area_square(3) == 9
 
   
 def test_calculate_area_square_negative():  
