208a8cbd461bd4feea87dd8601d7390e2114d836
N.Abdi <narimanabdi@gmail.com>
2025-01-28 09:07:57 +0330

    test

---
+++ tests/unit/test_area.py
@@ -0,0 +1,6 @@
+import pytest
+from src.area import calculate_area_square
+
+def test_calculate_area_square():
+    assert calculate_area_square(2) == 4
+    assert calculate_area_square(2.5) == 6.25
